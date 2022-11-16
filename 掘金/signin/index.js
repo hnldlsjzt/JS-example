aa79595bfb1ef2d0741b176de874999b2bcc72f5
小二上酒 <819163569@qq.com>
2022-11-16 17:19:25 +0800

    Update index.js

--- 掘金/signin/index.js
+++ 掘金/signin/index.js
@@ -1,7 +1,7 @@
 
 const shell = require('shelljs')
 //COOKIE要修改成你的cookie
-const COOKIE = `_ga=GA1.2.334489262.1621128817; OUTFOX_SEARCH_USER_ID_NCOO=1282695175.0854368; __tea_cookie_tokens_2608=^%^257B^%^2522web_id^%^2522^%^253A^%^25226998779962447414799^%^2522^%^252C^%^2522ssid^%^2522^%^253A^%^2522f2a5aa2c-e1ef-4c99-9473-c40bacf23dd7^%^2522^%^252C^%^2522user_unique_id^%^2522^%^253A^%^25226998779962447414799^%^2522^%^252C^%^2522timestamp^%^2522^%^253A1629530446389^%^257D; MONITOR_WEB_ID=d9711cf7-644a-4677-bd27-10a23460cf87; _tea_utm_cache_2608=^{^%^22utm_source^%^22:^%^22post_course_pay^%^22^}; passport_csrf_token=8333e41dd4053735b15f746fe477e3d1; passport_csrf_token_default=8333e41dd4053735b15f746fe477e3d1; _gid=GA1.2.132084547.1668521770; n_mh=P8OrmpgrO456QEpIM3hWn8HneCkOXa7F74gi4PqOMMM; uid_tt=c46cf599a5146ece95f721fe445f02f6; uid_tt_ss=c46cf599a5146ece95f721fe445f02f6; sid_tt=e9a58dc21849bb01326a16673d9e384a; sessionid=e9a58dc21849bb01326a16673d9e384a; sessionid_ss=e9a58dc21849bb01326a16673d9e384a; sid_guard=e9a58dc21849bb01326a16673d9e384a^%^7C1668521785^%^7C31535999^%^7CWed^%^2C+15-Nov-2023+14^%^3A16^%^3A24+GMT; sid_ucp_v1=1.0.0-KGFiYWRlMTliYTNhZjA0YTlhZjEwYmNjMTUzNmU3ZTIwY2I4MzAxNGEKFgit7eC__fXZARC5vs6bBhiwFDgIQAsaAmxmIiBlOWE1OGRjMjE4NDliYjAxMzI2YTE2NjczZDllMzg0YQ; ssid_ucp_v1=1.0.0-KGFiYWRlMTliYTNhZjA0YTlhZjEwYmNjMTUzNmU3ZTIwY2I4MzAxNGEKFgit7eC__fXZARC5vs6bBhiwFDgIQAsaAmxmIiBlOWE1OGRjMjE4NDliYjAxMzI2YTE2NjczZDllMzg0YQ`
+const COOKIE = `xxxx`
 
 // 签到
 function signIn() {
